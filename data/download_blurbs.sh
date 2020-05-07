ceb9d85ef6961ff400ab0254ca74869aedda6260
Karl Lundstig <karl@lundstig.com>
2020-05-07 17:41:38 +0200

    Fix blurb script including too many lines

--- data/download_blurbs.sh
+++ data/download_blurbs.sh
@@ -8,10 +8,10 @@ if ! [ -f blurbs.zip ]; then
   curl --output "blurbs.zip" "https://fiona.uni-hamburg.de/ca89b3cf/blurbgenrecollectionen.zip" 
 fi
 echo "Extracting zip..."
-unzip blurbs.zip
+unzip -o blurbs.zip
 echo "Reformatting data... (this takes some time)"
 cat BlurbGenreCollection_EN_dev.txt BlurbGenreCollection_EN_test.txt BlurbGenreCollection_EN_train.txt | \
-grep -E "title|body|isbn|d1" | \
+grep -E "<title>|<body>|<isbn>|<d0>" | \
 sed -r "s/<\w+>([^<]*)<\/\w+>/\1/" | \
 sed -r "s/<d1>/;/" | sed -r "s/<\/d1>//" > blurbs.txt
 rm -r __MACOSX
