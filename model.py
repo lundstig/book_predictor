a9a5e9b2e52a0cdbcec371cc800da5ac677cb7ec
Fredrik Hernqvist <hernqv@kth.se>
2020-05-16 18:32:31 +0200

    Cutoff words

--- model.py
+++ model.py
@@ -17,19 +17,21 @@ class Model(nn.Module):
         return output
 
 class BatchedModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layers=4):
+    def __init__(self, input_dim, hidden_dim, layers=4, max_words=100):
         super(BatchedModel, self).__init__()
 
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=layers, dropout=0.5)
         self.lstm_to_out = nn.Linear(hidden_dim, 1)
         self.input_dim = input_dim
+        self.max_words = max_words
 
     def forward(self, X):
         X_lengths = [len(x) for x in X]
-        longest = max(X_lengths)
-        padded_X = torch.zeros(len(X), longest, X[0].shape[1])
+        cutoff = min(self.max_words, max(X_lengths))
+        X_lengths = [min(len(x), cutoff) for x in X]
+        padded_X = torch.zeros(len(X), cutoff, X[0].shape[1])
         for i, x in enumerate(X):
-            padded_X[i][:X_lengths[i]] += x
+            padded_X[i][:X_lengths[i]] += x[:X_lengths[i]]
         X = nn.utils.rnn.pack_padded_sequence(padded_X, X_lengths, batch_first=True, enforce_sorted=False)
 
         lstm_output, (h_n, c_n) = self.lstm(X)
