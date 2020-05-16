Fix validation gradients and plot validation loss

learning.py
@@ -17,17 +17,20 @@ class Evaluator:
 
     def evaluate_model(self, model):
         model.eval()
-        ret = self.__evaluate(lambda x: model(x))
+        with torch.no_grad():
+            ret = self.__evaluate(model)
         model.train()
         return ret
 
     def evaluate_constant(self, k):
-        return self.__evaluate(lambda _: torch.tensor([[k]]))
+        with torch.no_grad():
+            return self.__evaluate(lambda _: torch.tensor([[k]]))
 
     def __evaluate(self, predict):
         total_loss = 0
-        for x, y in zip(self.X, self.Y):
-            total_loss += float(self.loss_function(predict(x), y))
+        with torch.no_grad():
+            for x, y in zip(self.X, self.Y):
+                total_loss += float(self.loss_function(predict(x), y))
         return total_loss/len(self.X)
 
 
@@ -74,15 +77,20 @@ def rnn_train(X, Y, learning_rate, epochs):
 
     return rnn, loss_history
 
-def train_model(X, Y, hidden_dim, learning_rate, epochs, evaluator=None):
+def train_model(X, Y, hidden_dim, learning_rate, epochs, evaluator=None, useSGD=True):
     n = len(X)
     input_dim = X[0].shape[1]
 
     loss_function = nn.MSELoss()
     model = Model(input_dim, hidden_dim)
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
-    loss_history = []
+    if useSGD:
+        optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+    else:
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    training_history = []
+    validation_history = []
     current_loss = 0
     for epoch in range(epochs):
         print(f"Epoch {epoch+1}/{epochs} ")
@@ -93,14 +101,17 @@ def train_model(X, Y, hidden_dim, learning_rate, epochs, evaluator=None):
 
             prediction = model(x)
             loss = loss_function(prediction, y)
-            current_loss += float(loss)
+            current_loss += float(loss) / n
 
             loss.backward()
             optimizer.step()
 
-        if not evaluator == None:
-            print("Validation loss:", evaluator.evaluate_model(model))
-        loss_history.append(current_loss / n)
+        training_history.append(current_loss)
+        print("Training loss:", current_loss)
         current_loss = 0
+        if evaluator:
+            val_loss = evaluator.evaluate_model(model)
+            print("Validation loss:", val_loss)
+            validation_history.append(val_loss)
 
-    return model, loss_history
+    return model, training_history, validation_history

main.py
@@ -6,8 +6,8 @@ print("Loading data...")
 X = torch.load("data/X_small.bin")
 Y = torch.load("data/Y_small.bin")
 
-TRAINING_PROPORTION = 0.2
-VALIDATION_PROPORTION = 0.1
+TRAINING_PROPORTION = 0.002
+VALIDATION_PROPORTION = 0.02
 assert TRAINING_PROPORTION + VALIDATION_PROPORTION < 1
 
 total_count = len(X)
@@ -21,10 +21,11 @@ trainingY = Y[:training_count]
 
 validationX = X[training_count:][:validation_count]
 validationY = Y[training_count:][:validation_count]
-evaluator = learning.Evaluator(validationX, validationY)
+with torch.no_grad():
+    evaluator = learning.Evaluator(validationX, validationY)
 
 def mean(Ys):
-  return sum(Ys) / len(Ys)
+    return sum(Ys) / len(Ys)
 
 def getMeanLoss(Ys):
     meanY = mean(Ys)
@@ -33,8 +34,10 @@ def getMeanLoss(Ys):
         loss += (y - meanY) ** 2
     return round(float(loss / len(Ys)), 4)
 
-print("Loss from guessing mean: ", getMeanLoss(trainingY))
-print("Evaluator loss on guessing mean:", evaluator.evaluate_constant(mean(trainingY)))
-model, loss_history = learning.train_model(trainingX, trainingY, 100, 0.01, 3, evaluator)
-print(loss_history)
-plotting.plot_loss_history(loss_history)
+print("Training loss from guessing mean: ", getMeanLoss(trainingY))
+print("Evaluator loss from guessing mean:", round(evaluator.evaluate_constant(mean(trainingY)), 4))
+model, training_loss, validation_loss = learning.train_model(trainingX, trainingY, 100, 0.001, 100, evaluator)
+print(training_loss)
+print(validation_loss)
+torch.save(model, "data/model.bin")
+plotting.plot_loss_history(training_loss, validation_loss)